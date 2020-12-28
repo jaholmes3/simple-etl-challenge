25e76db58ff5455ee927e07bf06d1adb50ad8865
Jim <jaholmes3@gmail.com>
2020-12-28 13:48:30 -0600

    moved resources file

--- transformer/util/__init__.py
+++ transformer/util/__init__.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append("/Users/admin/PycharmProjects/simple-etl-challenge/util")
+sys.path.append("/Users/admin/PycharmProjects/simple-etl-challenge/transformer")
 
 from load_sample import load_sample_file
 from validate_output import validate_json
